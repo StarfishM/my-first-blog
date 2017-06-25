e22966d37f40f9fd2dcf8dfe378410613d6db415
merle <merle_canada@hotmail.com>
2017-06-25 21:38:40 +0200

    made necessary changes so that post are saved as drafts before being published.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -28,7 +28,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -43,7 +42,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
