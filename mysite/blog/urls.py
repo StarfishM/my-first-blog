7bc835161bff6f4a1bc91f1738fe9224389432db
merle <merle_canada@hotmail.com>
2017-06-30 19:25:12 +0200

    still unfixed login issues

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^admin$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,8 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
-
-# Create your views here.
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import render
@@ -16,6 +11,16 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+
+# Create your views here.
+def login(request):
+    return render(request, 'registration/login.html', {})
+
+    
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
