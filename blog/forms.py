f96eb4cea03b453ca2402186c3136a796c00e3dc
merle <merle_canada@hotmail.com>
2017-07-24 21:43:07 +0200

    added language selection field to blog posts and edits

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', 'language')
 
 class CommentForm(forms.ModelForm):
 
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+
+LANGUAGES = (("EN", "English"), ("DE", "Deutsch"))
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
+    language = models.CharField(choices=LANGUAGES, default="EN", max_length=20)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
