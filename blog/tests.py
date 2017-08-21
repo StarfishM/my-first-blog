4e299a15391e1044eebb87fe0961844349836263
merle <merle_canada@hotmail.com>
2017-08-21 20:57:42 +0200

    Change gitignore and add test template

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+# from blog.models import Post
+
+# import django
+
+
+# class LandingTests(TestCase):
+#    def test_show_english_posts_on_landing(self):
+#        django.setup()
+#        Post.objects.all()
+
+#        print('yayyyy')
